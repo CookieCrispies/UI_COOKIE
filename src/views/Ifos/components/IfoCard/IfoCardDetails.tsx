7475215baff340ef4ef001bb2c39a1f6aefb8621
Cookiedev <cookiebasedev@gmail.com>
2023-08-14 16:46:47 +0200

    update

--- src/views/Ifos/components/IfoCard/IfoCardDetails.tsx
+++ src/views/Ifos/components/IfoCard/IfoCardDetails.tsx
@@ -77,7 +77,7 @@ const IfoCardDetails: React.FC<IfoCardDetailsProps> = ({
       {/* <LinkExternal href={projectSiteUrl} style={{ margin: 'auto' }}>
         {TranslateString(412, 'View project site')}
       </LinkExternal> */}
-      <LinkExternal href="https://basescan.org//address/" style={{ margin: 'auto' }}>
+      <LinkExternal href="https://basescan.org/address/0x5a1df64163ac8e5699697dbf5609c7d228cdb63c#code" style={{ margin: 'auto' }}>
         {TranslateString(412, 'View contract address')}
       </LinkExternal>
     </>
